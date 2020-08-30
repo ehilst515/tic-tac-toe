7c7c777dd598db94afa95ddd6303f395ece8831e
Enrique Hilst <enrique.hilst@gmail.com>
2020-08-30 16:21:20 -0500

    game working

--- Tic-Tac-Toe/Position.cs
+++ Tic-Tac-Toe/Position.cs
@@ -1,10 +1,23 @@
 ﻿using System;
-namespace Tic_Tac_Toe
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04_TicTacToe.Classes
 {
-    public class Position
-    {
-        public Position()
-        {
-        }
-    }
+	class Position
+	{
+		public int Row { get; set; }
+		public int Column { get; set; }
+
+		/// <summary>
+		/// Position on the gameboard being initialized 
+		/// </summary>
+		/// <param name="row">row number</param>
+		/// <param name="column">column number</param>
+		public Position(int row, int column)
+		{
+			Row = row;
+			Column = column;
+		}
+	}
 }
--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using Lab04_TicTacToe.Classes;
 
-namespace Tic_Tac_Toe
+namespace Lab04_TicTacToe
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            StartGame();
+        }
+
+        static void StartGame()
+        {
+            // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+            // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
+            Console.WriteLine("Player 1, enter your name: ");
+            string p1Name = Console.ReadLine();
+            Console.WriteLine("Player 2, enter your name: ");
+            string p2Name = Console.ReadLine();
+            Player p1 = new Player();
+            Player p2 = new Player();
+            p1.Name = p1Name;
+            p2.Name = p2Name;
+            p1.Marker = "X";
+            p2.Marker = "O";
+            Game newGame = new Game(p1, p2);
+            Player winner = newGame.Play();
+            if(winner.Name != "Draw")
+            {
+                Console.WriteLine($"Winner: {winner.Name}");
+            }
+            else
+            {
+                Console.WriteLine("Draw");
+            }
         }
     }
 }
